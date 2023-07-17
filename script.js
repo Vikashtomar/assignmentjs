ab0b07d6cc2e97297395dde4155da5597a17cea9
vikash <vikash.tomar.india@gmail.com>
2023-07-17 14:12:03 +0530

    commit third

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-  let value = [1,2,3,4,5];
+  // let value = [1,2,3,4,5];
   
 
-  const {first, ...rest} = value
-  console.log(rest);
-  // destructuring
+  // const {first, ...rest} = value
+  // console.log(rest);
+  
+
+
+  
+let x = true;
+let y = "true";
+
+if(x != y) {
+  console.log("hello");
+}
+else{
+  console.log("Working in else")
+}
+
+
