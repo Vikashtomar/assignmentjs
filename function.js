bfa164ea810fc8f5433a58636d36eb7db549c4eb
vikash <vikash.tomar.india@gmail.com>
2023-07-26 10:54:28 +0530

    second

---
+++ function.js
@@ -0,0 +1,18 @@
+
+
+// function sum(a,b){
+
+//     return a*b;
+// }
+// //   console(sum(2,5));
+//   console.log(sum(2,6))
+
+
+//  Anonnomus function
+
+
+  let hello = function (){
+    console.log("Hello world!");
+
+  }
+  hello()
---
+++ loops.js
@@ -0,0 +1,25 @@
+//  let i=0; 
+//  while(i<=10){
+//     console.log(i);
+//     i = i*3
+//  }i++;
+
+// let i = 0;
+// while(i<=10)
+// {
+//     if(i%2 !==0){
+//         console.log(i)
+//     }
+//     i++;
+// }
+
+
+let a = 1;
+
+do{
+    // console.log(i)
+
+    document.write("hello")
+    document.write("<br>")
+    a = a+1
+}while (a <=10 )
