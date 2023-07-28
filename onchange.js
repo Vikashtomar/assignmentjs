af0bb6be6be3d47c87b79f2264123998357c3d38
vikash <vikash.tomar.india@gmail.com>
2023-07-28 22:18:12 +0530

    commit added

---
+++ onchange.js
@@ -0,0 +1,7 @@
+
+
+let inp = document.querySelector("input");
+console.log(inp)
+
+
+let filldata = document.qs
--- script.js
+++ script.js
@@ -27,9 +27,31 @@
 
 
 
-let nums = [1,2,3,4,5]
+// let nums = [1,2,3,4,5]
 
-let result = nums.map((num)=>{
-  return num*num
-})
-console.log('result',result )
+// let result = nums.map((num)=>{
+//   return num*num
+// })
+// console.log('result',result )
+
+
+
+//Get age from dob
+function calculateAge(dob) {
+  const dobDate = new Date(dob);
+  const today = new Date();
+
+  let age = today.getFullYear() - dobDate.getFullYear();
+  const monthDiff = today.getMonth() - dobDate.getMonth();
+
+  if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dobDate.getDate())) {
+    age--; // If the birth date has not yet been reached this year, subtract 1 from age
+  }
+
+  return age;
+}
+
+// Example usage:
+const dob = "2000-01-15";
+const age = calculateAge(dob);
+console.log(age); // Output will be the current age based on the provided DOB
