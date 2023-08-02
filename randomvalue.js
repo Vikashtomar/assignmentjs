192d17679e3f1b202b67c10745ed9a44faea28a5
vikash <vikash.tomar.india@gmail.com>
2023-08-02 23:23:25 +0530

    modified

--- randomvalue.js
+++ randomvalue.js
@@ -1,4 +1,4 @@
 
-const nums = [ 55, 56, 34, 25, 23, 59, 78, 88, 97, 76] 
+// const nums = [ 55, 56, 34, 25, 23, 59, 78, 88, 97, 76] 
 
- document.write(nums[Math.floor(Math.random()*nums.length)]);
+//  document.write(nums[Math.floor(Math.random()*nums.length)]);
