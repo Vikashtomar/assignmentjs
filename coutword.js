2b6b1076c84c342ab6a8195971a8176341eab453
vikash <vikash.tomar.india@gmail.com>
2023-08-06 11:00:53 +0530

    count word

---
+++ coutword.js
@@ -0,0 +1,18 @@
+  
+        function countWordOccurrences(sentence,word)
+        {
+          let s = sentence.toLowerCase().split(" ");
+          let w = word.toLowerCase()
+          let result = 0;
+          for(let i=0; i<sentence.length; i++){
+            if((s[i] == w)){
+              // console.log(result)
+               result++
+
+            }
+       
+            
+          }
+          console.log(result);
+        }
+        countWordOccurrences("this is Hello hello hello","hello")
